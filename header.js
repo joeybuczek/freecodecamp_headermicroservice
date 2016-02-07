926fd761ba7836b36d5e5a795a5b8082291bea5a
Joey Buczek <joe.buczek@gmail.com>
2016-02-07 20:25:21 +0000

    udpated with html

--- header.js
+++ header.js
@@ -9,21 +9,43 @@ var ip;
 request("http://www.l2.io/ip", function(error, response, body){
     if (error) throw error;
     ip = body;
-    console.log(ip);
 });
 
 // server
 var app = express();
 
+// create response object
+var responseObj = {};
+
+// index html
+var html = "<h2>Header Parser Microservice</h2>" + 
+           "<p>This microservice will supply your IP address and browser specifics in a JSON stringified format.</p>" +
+           "<p>Point your http GET request here: " + 
+            "<a href='http://joeybuczek.herokuapp.com/fcc-headermicroservice/whoami'>" + 
+            "http://joeybuczek.herokuapp.com/fcc-headermicroservice/whoami</a></p>";
+
+// route /
+app.get('/', function (req, res){
+    res.send(html);
+});
+
+// route /whoami
+app.get('/whoami', function (req, res) {
     
+    // assign ip
+    responseObj.ip = ip;
     
-
+    // assign language
+    responseObj.language = navigator.language || "en";
     
-
-
-
-
-
+    // OS 
+    var appver = navigator.appVersion;
+    var os = appver.slice( appver.indexOf("\(") + 1, appver.indexOf("\)") );
+    responseObj.software = os;
+    
+    // send response object
+    res.send(JSON.stringify(responseObj));
+});
 
 // listen
 var port = process.env.port || 8080;
